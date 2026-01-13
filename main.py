13397cf2838c83ab1a2636f976a37370931a72ac
MoronDe <sans22344@mail.ru>
2026-01-14 01:36:29 +0600

    Fix Cooldown

--- main.py
+++ main.py
@@ -74,7 +74,6 @@ def load_prompt(character="Crazy Mita", language="EN"):
     with open(path, "r", encoding="utf-8") as f:
         return f.read()
 
-"""
 @app.before_request
 def apply_cooldown():
     if request.endpoint == "ask":
@@ -84,8 +83,6 @@ def apply_cooldown():
         if now - last_time < COOLDOWN:
             return jsonify({"error": f"Cooldown {COOLDOWN} sec"}), 429
         last_request_time[ip] = now
-        
-"""
 
 @app.route('/ask', methods=['POST'])
 def ask():
